c8b99ac4eba4d644b9d326acb222039e9a34ef0c
Csaba Kadlecsik <cskadlecsik@gmail.com>
2017-05-19 14:59:31 +0200

    initial commit

---
+++ src/main/java/hu/elte/projekteszkozok/NoteappApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.projekteszkozok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteappApplication.class, args);
+	}
+}
